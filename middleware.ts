72d19e5fd8f460e868aca049d39c21147e4c1721
Steven <58159084+Auxdible@users.noreply.github.com>
2023-08-18 16:34:54 -0400

    fix: middleware function export?

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,13 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
 
+export default withAuth(
+    function middleware(req) {
+      
+    },
+    {
+      callbacks: {
+        authorized: ({ token }) => (token?.guilds?.length || 0) > 0,
+      },
+    }
+  )
 export const config = { matcher: ["/dashboard"] }
