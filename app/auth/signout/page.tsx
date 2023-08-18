3860e77c12fd8fce6d66457bf78107359d036c4a
Steven <58159084+Auxdible@users.noreply.github.com>
2023-08-18 16:59:18 -0400

    fix: center text

--- app/auth/signout/page.tsx
+++ app/auth/signout/page.tsx
@@ -11,8 +11,8 @@ export default function SignIn() {
     return (
     <main className={"flex justify-center flex-col items-center gap-5 bg-gray-700 flex-grow"}>
         {error ? <div className={"bg-red-600 border-gray-500 border-opacity-20 border-2 flex flex-col py-2 px-5 rounded-3xl font-roboto gap-2"}><span className={"flex flex-row gap-2 items-center"}><BsExclamationCircle/> An error occurred</span><span>Couldn&apos;t sign you into Auxdibot.</span><span>Error Code: <i>{error}</i></span></div> : ""}
-        <h1 className={"header text-6xl"}>Sign In</h1>
-        <p className={"secondary text-2xl"}>Sign in to use Auxdibot&apos;s dashboard!</p>
+        <h1 className={"header text-6xl text-center"}>Sign In</h1>
+        <p className={"secondary text-2xl text-center"}>Sign in to use Auxdibot&apos;s dashboard!</p>
         <div className={"bg-gray-800 bg-opacity-50 p-4 rounded-2xl"}><span onClick={() => signOut({ 'callbackUrl': '/' })}><Button text={"Sign out"} icon={<BsArrowLeftCircle/>}/></span></div>
     </main>)
 }
