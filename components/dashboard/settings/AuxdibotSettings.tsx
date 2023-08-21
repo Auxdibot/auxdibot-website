a7d33e6f27f3227142f205e53f029f214f2ed472
Steven <58159084+Auxdible@users.noreply.github.com>
2023-08-21 14:24:50 -0400

    feat: add in nickname field to dashboard

--- components/dashboard/settings/AuxdibotSettings.tsx
+++ components/dashboard/settings/AuxdibotSettings.tsx
@@ -1,21 +1,24 @@
 "use client";
 
+import Button from "@/components/Button";
 import useSession from "@/lib/hooks/useSession";
 import DiscordGuild from "@/lib/types/DiscordGuild";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
-import { BsArrowLeftCircle, BsThreeDots, BsToggles, BsTrash } from "react-icons/bs";
+import { BsArrowLeftCircle, BsPersonBadge, BsThreeDots, BsToggles, BsTrash } from "react-icons/bs";
 import { useQueryClient } from "react-query";
+import NicknameChange from "./NicknameChange";
 
 export default function AuxdibotSettings({ server }: { server: DiscordGuild & { data: {serverID: string, disabled_modules: string[]} } }) {
     const queryClient = useQueryClient();
     const router = useRouter();
-    const { user } = useSession();
+    
     const [confirmation, setConfirmation] = useState(false);
     const [shake, setBarShake] = useState(false);
     const [loading, setLoading] = useState(false);
+    
     const serverNameRef = useRef<HTMLInputElement>(null);
-    function handleSubmit(e: React.MouseEvent<HTMLButtonElement>) {
+    function reset(e: React.MouseEvent<HTMLButtonElement>) {
         e.preventDefault();
         if (!server) return;
         if (serverNameRef.current?.value != server?.name) {
@@ -32,11 +35,14 @@ export default function AuxdibotSettings({ server }: { server: DiscordGuild & { 
     }
     
     return <>
-    <div className={"bg-gray-800 shadow-2xl border-2 border-gray-800 rounded-2xl h-fit w-fit max-md:mx-auto"}>
+    <div className={"bg-gray-800 shadow-2xl border-2 border-gray-800 rounded-2xl h-fit w-full max-md:mx-auto"}>
     <h2 className={"bg-gray-900 secondary text-2xl p-4 text-center rounded-2xl rounded-b-none"}>Bot Settings</h2>
+    <div className={"flex flex-col gap-4"}>
+    <NicknameChange server={server}/>
     <button onClick={() => setConfirmation(!confirmation)} className={"secondary my-5 mx-auto hover:bg-gradient-to-l hover:from-red-400 hover:to-red-700 hover:text-black text-lg hover:border-black transition-all w-fit border-white border rounded-xl p-1 flex flex-row gap-2 items-center"} type="submit">
         <BsTrash/> Reset Bot
     </button>
+    </div>
     </div>
     {confirmation ? <div className={"fixed w-screen h-screen top-0 left-0 bg-black bg-opacity-50 flex justify-center items-center"}>
         <div className={"bg-gray-700 rounded-2xl max-w-lg items-center flex flex-col text-center p-5 gap-5 max-md:gap-3"}>
@@ -44,7 +50,7 @@ export default function AuxdibotSettings({ server }: { server: DiscordGuild & { 
             <p className={"text-2xl max-md:text-lg font-roboto"}>You are about to completely reset <span className={"font-bold text-red-500"}>EVERY</span> setting you have on Auxdibot for this server, and revert Auxdibot back to its default state. Are you sure?<br/><br/>Type <code>{server?.name}</code> below to confirm.</p>
             <input className={`${shake ? "animate-incorrect animate" : ""} text-white text text-lg max-md:text-sm rounded-md border border-gray-500 p-1`} ref={serverNameRef}/>
             <span className={"w-full flex flex-row justify-between"}>
-            <button onClick={(e) => handleSubmit(e)} className={"secondary text-2xl max-md:text-lg hover:bg-gradient-to-l hover:from-red-400 hover:to-red-700 hover:text-black hover:border-black transition-all w-fit border-white border rounded-xl p-1 flex flex-row gap-2 items-center"} type="submit">
+            <button onClick={(e) => reset(e)} className={"secondary text-2xl max-md:text-lg hover:bg-gradient-to-l hover:from-red-400 hover:to-red-700 hover:text-black hover:border-black transition-all w-fit border-white border rounded-xl p-1 flex flex-row gap-2 items-center"} type="submit">
                 <BsTrash/> Reset Bot
             </button>
             <button onClick={(e) => setConfirmation(false)} className={"secondary text-2xl max-md:text-lg hover-gradient hover:text-black hover:border-black transition-all w-fit border-white border rounded-xl p-1 flex flex-row gap-2 items-center"} type="submit">
---
+++ components/dashboard/settings/NicknameChange.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { BsCheck, BsCheckLg, BsPersonBadge } from "react-icons/bs";
+import { useState } from 'react'; 
+import DiscordGuild from "@/lib/types/DiscordGuild";
+import { useQueryClient } from "react-query";
+export default function NicknameChange({ server }: { server: DiscordGuild & { data: {serverID: string, disabled_modules: string[]} } }) {
+    const [nick, setNick] = useState("");
+    const [success, setSuccess] = useState(false);
+    const queryClient = useQueryClient();
+    function onNickChange(e: React.ChangeEvent<HTMLInputElement>) {
+        if (success) setSuccess(false);
+        setNick(e.currentTarget.value);
+    }
+    function setNickname() {
+        if (!server) return;
+        const body = new URLSearchParams();
+        body.append("new_nickname", nick);
+        fetch(`/api/v1/servers/${server.id}/nick`, { method: "POST", body }).then(() => {
+            queryClient.invalidateQueries(["data_settings", server.id])
+            setSuccess(true)
+            setNick("");
+        }).catch(() => {});
+    }
+    return <div className={"flex flex-col gap-3 w-fit mx-auto border-b p-4 border-gray-700"}>
+    <span className={"secondary text-xl text-center flex flex-col"}>Change Bot Nickname <span className={"text text-gray-500 italic text-sm text-center"}>(Leave empty to reset, max length 32 characters)</span></span>
+    
+    <span className={"flex flex-row max-md:flex-col gap-2"}><input value={nick} onChange={(e) => onNickChange(e)} className={"font-roboto rounded-md p-0 text-md"}/> 
+        <button onClick={() => setNickname()} className={`secondary text-md max-md:mx-auto ${success ? "bg-gradient-to-l from-green-400 to-green-600 text-black border-black" : "hover-gradient border-white"} hover:text-black hover:border-black transition-all w-fit  border rounded-xl p-1 flex flex-row gap-2 items-center`} type="submit">
+            {success ? (<><BsCheckLg/> Updated!</>) : (<><BsPersonBadge/> Change Name</>) }
+        </button></span>
+    </div>
+}
