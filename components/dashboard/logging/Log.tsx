111504aae035003e40f654c05d3c8afa4ce044dd
Steven <58159084+Auxdible@users.noreply.github.com>
2023-12-26 09:26:04 -0500

    feat: working on moderator dashboard and tweaked table to allow description

--- components/dashboard/logging/Log.tsx
+++ components/dashboard/logging/Log.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import useMousePosition from "@/hooks/useMousePosition";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import { useQuery } from "react-query";
 
 export default function Log({ log }: { log: { type: string, userID: string, date_unix: number, description: string }}) {
     const { data: user } = useQuery(["user", log?.userID], async () => await fetch(`/api/v1/user?id=${log?.userID}`).then(async (res) => await res.json()).catch(() => undefined));
-    return <tr className={"border w-full max-md:w-full flex justify-between items-center max-md:flex-col"}>
+    const [hovered, setHovered] = useState(false);
+    const { x, y } = useMousePosition(); 
+    useEffect(() => {
+        if (hovered && tooltipRef && tooltipRef.current) {
+            tooltipRef.current.setAttribute('style', `top: ${y ? y - 20 : 0}px; left: ${x ? x + 20 : 0}px;`)
+        }
+    }, [hovered, x, y])
+    const tooltipRef = useRef<HTMLSpanElement | null>(null);
+    return <tr onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className={"border w-full max-md:w-full flex justify-between items-center max-md:flex-col group"}>
+        <span className={"absolute group-hover:scale-100 scale-0 origin-center font-open-sans p-2 rounded-2xl border border-gray-600 bg-gray-800 max-w-xs italic"} ref={tooltipRef}>
+            
+            {log.description}</span>
         <td className="flex-1">{log.type}</td> 
         <td className="flex-1 justify-center flex items-center gap-1">
         { user?.avatar ? <Image
