7f0e53c07b9d27bfb97aa4dd2d829b776b77ce79
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-04 16:43:17 -0500

    feat: updated starboard dashboard with new components

--- components/dashboard/starboard/StarboardChannel.tsx
+++ components/dashboard/starboard/StarboardChannel.tsx
@@ -4,6 +4,7 @@ import { BsCheckLg, BsStar } from "react-icons/bs";
 import { useContext, useState } from 'react'; 
 import { useQuery, useQueryClient } from "react-query";
 import DashboardActionContext from "@/context/DashboardActionContext";
+import Channels from "@/components/input/Channels";
 export default function StarboardChannel({ server }: { server: { serverID: string }}) {
     let { data: channels } = useQuery(["data_channels", server.serverID], async () => await fetch(`/api/v1/servers/${server.serverID}/channels`).then(async (data) => 
     await data.json().catch(() => undefined)).catch(() => undefined));
@@ -11,22 +12,20 @@ export default function StarboardChannel({ server }: { server: { serverID: strin
     const [success, setSuccess] = useState(false);
     const actionContext = useContext(DashboardActionContext);
     const queryClient = useQueryClient();
-    function onStarboardChannelChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    function onStarboardChannelChange(e: { channel: string | null }) {
         if (success) setSuccess(false);
-        if (e.currentTarget.value == "null") return;
-
-        setChannel(e.currentTarget.value);
+        setChannel(e.channel || '');
     }
     function setStarboardChannel() {
         if (!server) return;
         const body = new URLSearchParams();
-        body.append("starboard_channel", channel);
+        body.append("starboard_channel", channel || '');
         fetch(`/api/v1/servers/${server.serverID}/starboard/channel`, { method: "POST", body }).then(() => {
             queryClient.invalidateQueries(["data_starboard", server.serverID])
             setSuccess(true)
             setChannel("");
             if (actionContext)
-                actionContext.setAction({ status: `Successfully updated starboard channel to: ${channels.find((c: { id: string }) => channel == c.id)?.name || "None. Starboard is disabled."}`, success: true });
+                actionContext.setAction({ status: `Successfully updated starboard channel to: #${channels.find((c: { id: string }) => channel == c.id)?.name || "None. Starboard is disabled."}`, success: true });
         }).catch(() => {     
         });
     }
@@ -36,10 +35,7 @@ export default function StarboardChannel({ server }: { server: { serverID: strin
     <span className={"secondary text-xl text-center flex flex-col"}>Set Starboard Channel</span>
     
     <span className={"flex flex-row max-xl:flex-col items-center gap-2"}>
-        <select onChange={(e) => onStarboardChannelChange(e)} value={channel} className={"font-roboto w-fit mx-auto rounded-md p-1 text-md"}>
-            <option value={"null"}>Select a channel...</option>
-            {channels?.map((i: { id: string, name: string }) => <option key={i.id} value={i.id}>{i.name}</option>)}
-        </select> 
+        <Channels serverID={server.serverID} value={channel} onChange={onStarboardChannelChange}/>
         <button onClick={() => setStarboardChannel()} className={`secondary text-md max-md:mx-auto ${success ? "bg-gradient-to-l from-green-400 to-green-600 text-black border-black" : "hover-gradient border-white"} hover:text-black hover:border-black transition-all w-fit border rounded-xl p-1 flex flex-row gap-2 items-center`} type="submit">
             {success ? (<><BsCheckLg/> Updated!</>) : (<><BsStar/> Change Channel</>) }
         </button></span>
