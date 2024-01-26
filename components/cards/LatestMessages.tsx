3f594854759a8c73d3f8d76a3dc095be313d943a
Steven <58159084+Auxdible@users.noreply.github.com>
2024-01-26 10:32:44 -0500

    fix: cards off center, use flex-1 containers

--- components/cards/LatestMessages.tsx
+++ components/cards/LatestMessages.tsx
@@ -8,8 +8,8 @@ export default function LatestMessages({ data }: { readonly data: CardData; }) {
 
     const header = CardFonts[data?.header_font || "BAUHAUS_93"], text = CardFonts[data?.text_font || 'OPEN_SANS'];
 
-    return <div className={"flex justify-center items-center max-w-sm"}>
-    <section className={`${data?.dark ? "bg-black" : "bg-gray-100"} ${data?.dark ? "text-gray-100" : "text-gray-900"} ${data?.dark ? "border-gray-800" : "border-gray-300"} border max-w-full px-3 py-10 bg-opacity-60 rounded-2xl`}>
+    return <div className={"w-full flex justify-center items-center max-w-sm"}>
+    <section className={`${data?.dark ? "bg-black" : "bg-gray-100"} ${data?.dark ? "text-gray-100" : "text-gray-900"} ${data?.dark ? "border-gray-800" : "border-gray-300"} border w-full max-w-full px-3 py-10 bg-opacity-60 rounded-2xl`}>
     <div className={`flex flex-col justify-center items-center gap-5 font-${text}`}>
         <h1 className={`text-3xl font-${header} flex flex-col justify-center items-center`}>Latest Messages<br/><span className={"flex items-center gap-2 break-words-wrap max-w-full"}><BsHash/> {data.channel?.name}</span></h1>
         <ul className={"flex flex-col max-w-full"}>
