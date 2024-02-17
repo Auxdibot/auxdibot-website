3a1fef4d3439d4eaf1a85e2f89a83a0856d3b621
Steven <58159084+Auxdible@users.noreply.github.com>
2024-02-17 14:05:41 -0500

    fix: name positioning

--- components/home/Masthead.tsx
+++ components/home/Masthead.tsx
@@ -44,9 +44,9 @@ export default function Masthead() {
         
      
             <h1 className={"header text-8xl max-md:text-6xl w-fit"}>auxdibot</h1>
-            <p className={"secondary text-3xl max-md:text-2xl text-white text-center flex flex-col items-center my-5"}>The next Discord Bot for your 
+            <span className={"secondary text-3xl max-md:text-2xl text-white text-center flex flex-col items-center"}>The next Discord Bot for your 
             <Names/>
-            </p>
+            </span>
             <Analytics/>
             {status == 'loading' ? <BsThreeDots className={"animate-spin text-4xl text-white"}/> : user ? <Button icon={<BsGear/>} text={"Dashboard"} href={"/dashboard"}/> : <Button href={process.env.NEXT_PUBLIC_DISCORD_INVITE_LINK} icon={<BsEnvelopePlus/>} text={"Invite Bot"}/> }
         </div>
--- components/home/Names.tsx
+++ components/home/Names.tsx
@@ -13,13 +13,13 @@ export default function Names() {
               if (text2 <= text1) {
                 if (text1 >= names.length-1) setTimeout(() => setText1(0), 750);
                 setText2((text1+1) > names.length-1 ? 0 : text1+1);
-                text2Ref.current?.animate({ transform: ['translateY(-96px)', 'translateY(-48px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' })
-                text1Ref.current?.animate({ transform: ['translateY(0)', 'translateY(48px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' })
+                text2Ref.current?.animate({ opacity: ['0', '1'], transform: ['translateY(-112px)', 'translateY(-48px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' })
+                text1Ref.current?.animate({ opacity: ['1', '0'], transform: ['translateY(0)', 'translateY(56px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' })
               } else if (text1 <= text2) {
                 if (text2 >= names.length-1) setTimeout(() => setText2(0), 750);
                 setText1((text2+1) > names.length-1 ? 0 : text2+1);
-                text1Ref.current?.animate({ transform: ['translateY(-48px)', 'translateY(0px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' })
-                text2Ref.current?.animate({ transform: ['translateY(-48px)', 'translateY(0px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' });
+                text1Ref.current?.animate({ opacity: ['0', '1'], transform: ['translateY(-56px)', 'translateY(0px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' })
+                text2Ref.current?.animate({ opacity: ['1', '0'], transform: ['translateY(-56px)', 'translateY(0px)'] }, { duration: 400, easing: 'ease-in-out', fill: 'forwards' });
             }  
           
 
@@ -32,8 +32,8 @@ export default function Names() {
         return () => clearInterval(interval);
     }, [text1, text2]);
 
-    return <div className="flex flex-col overflow-hidden h-12 w-fit">
-    <span className={"header text-5xl"} ref={text1Ref}>{names[text1]}</span>
-    <span className={"header text-5xl"} ref={text2Ref}>{names[text2]}</span>
+    return <div className="flex flex-col overflow-hidden h-14 w-fit">
+    <span id="test1" className={"header text-5xl h-14"} ref={text1Ref}>{names[text1]}</span>
+    <span className={"header text-5xl h-14"} ref={text2Ref}>{names[text2]}</span>
   </div>;
 }
