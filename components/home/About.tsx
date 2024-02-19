8b9bee30f93c637ab37557f1bbc74253e81d029f
Steven <58159084+Auxdible@users.noreply.github.com>
2024-02-19 07:59:54 -0500

    feat: rudamentary features

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -9,17 +9,8 @@ import Start from './Start';
 export default function About() {
     
     return (
-        <section className={"w-full flex flex-col bg-gray-950 gap-40"}>
-        <section className={"bg-gradient-to-b from-black gap-20 flex flex-col pt-10 gap-40"}>
-        <section className={"max-w-4xl mx-auto flex max-md:flex-col gap-10 max-md:overflow-x-hidden"}>
-        <motion.h1 initial={{ transform: "translateX(-4rem)", opacity: 0 }} viewport={{ once: true }} whileInView={{ transform: "translateY(0px)", opacity: 1 }} transition={{ duration: 0.5 }} className={"text-6xl text-end max-md:text-center max-md:mx-auto items-end max-md:items-center flex flex-col justify-center lowercase"}><span className={"header"}>About</span><span className={"header"}>Auxdibot</span></motion.h1>
-        <motion.span initial={{ scaleY: 0 }} viewport={{ once: true }} whileInView={{ scaleY: 1 }} className={"flex-grow w-1 border-r-2 border-gray-400 max-md:hidden"}></motion.span>
-        <motion.p initial={{ transform: "translateX(4rem)", opacity: 0 }} viewport={{ once: true }} whileInView={{ transform: "translateY(0px)", opacity: 1 }} transition={{ duration: 0.5 }} className={'font-open-sans text-gray-400 text-2xl max-md:text-center'}>Auxdibot is a Discord bot project founded and maintained by <Link href={"https://auxdible.me"} className={"header font-montserrat font-bold"}>Auxdible</Link>. Auxdibot features a wide variety of features for admins to manage their servers with. Auxdibot receives consistant updates and constant bug fixes, making it a reliable choice for your server!</motion.p>
-        </section>
-        <Points/>
-        </section>
-        <Start/>
-        <section className={"max-w-6xl mx-auto flex flex-col my-20"}>
+        <section className={"w-full flex flex-col bg-gradient-to-b from-black to-background-300 gap-40"}>
+        <section className={"max-w-7xl w-full max-md:w-fit mx-auto flex flex-col my-20"}>
         
         <div className={"font-montserrat font-bold flex flex-col mx-auto text-5xl max-md:text-4xl text-center my-20 gap-5 py-1"}>
             <motion.h1 initial={{ transform: "translateY(-4rem)", opacity: 0 }} viewport={{ once: true }} whileInView={{ transform: "translateY(0px)", opacity: 1 }} transition={{ duration: 0.5 }} 
@@ -34,6 +25,16 @@ export default function About() {
         </div>
         <Features/>
         </section>
+        <section className={"flex flex-col pt-10 gap-40"}>
+        <section className={"max-w-4xl mx-auto flex max-md:flex-col gap-10 max-md:overflow-x-hidden"}>
+        <motion.h1 initial={{ transform: "translateX(-4rem)", opacity: 0 }} viewport={{ once: true }} whileInView={{ transform: "translateY(0px)", opacity: 1 }} transition={{ duration: 0.5 }} className={"text-6xl text-end max-md:text-center max-md:mx-auto items-end max-md:items-center flex flex-col justify-center lowercase"}><span className={"header"}>About</span><span className={"header"}>Auxdibot</span></motion.h1>
+        <motion.span initial={{ scaleY: 0 }} viewport={{ once: true }} whileInView={{ scaleY: 1 }} className={"flex-grow w-1 border-r-2 border-gray-400 max-md:hidden"}></motion.span>
+        <motion.p initial={{ transform: "translateX(4rem)", opacity: 0 }} viewport={{ once: true }} whileInView={{ transform: "translateY(0px)", opacity: 1 }} transition={{ duration: 0.5 }} className={'font-open-sans text-gray-400 text-2xl max-md:text-center'}>Auxdibot is a Discord bot project founded and maintained by <Link href={"https://auxdible.me"} className={"header font-montserrat font-bold"}>Auxdible</Link>. Auxdibot features a wide variety of features for admins to manage their servers with. Auxdibot receives consistant updates and constant bug fixes, making it a reliable choice for your server!</motion.p>
+        </section>
+        <Points/>
+        </section>
+        <Start/>
+        
         </section>
     );
 }
