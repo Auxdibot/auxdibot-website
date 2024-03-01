2e1f2740241de957ae828853d6bc27a27c9894af
Steven <58159084+Auxdible@users.noreply.github.com>
2024-03-01 08:18:17 -0500

    chore: organize card into files

---
+++ components/dashboard/cards/CardBody.ts
@@ -0,0 +1,4 @@
+"use client";
+import { CardData } from '@/lib/types/CardData';
+
+export type CardBody = Omit<CardData, 'rules' | 'channel'> & { rulesField: { rule: string; }[]; } & { channelID: string; };
