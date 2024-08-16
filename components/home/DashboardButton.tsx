22d920bd3fab8b30d037fab546a784088e9ad912
Steven Primeaux <stevenhotrod06@gmail.com>
2024-08-16 19:56:33 +0000

    fix: typo in auth link causing everything to break

--- components/home/DashboardButton.tsx
+++ components/home/DashboardButton.tsx
@@ -8,5 +8,5 @@ export function DashboardButton() {
     const { status } = useSession();
     return status == 'authenticated' ?
     <HeaderButton className="font-bold flex text-2xl" href={'/dashboard'}><span className="flex items-center gap-2"><Settings/> Dashboard</span></HeaderButton>
-    : <HeaderButton className="font-bold flex text-2xl" href={'/bot/v1/discord'}><span className="flex items-center gap-2"><User/> Sign In</span></HeaderButton>
+    : <HeaderButton className="font-bold flex text-2xl" href={'/bot/v1/auth/discord'}><span className="flex items-center gap-2"><User/> Sign In</span></HeaderButton>
 }
