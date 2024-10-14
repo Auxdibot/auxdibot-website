242f05af905f5e3c0c3b5ff81b8e82bcf2fa8a32
Steven <58159084+Auxdible@users.noreply.github.com>
2024-10-14 12:01:04 -0400

    feat(premium): update benefits

--- components/premium/PremiumBenefits.tsx
+++ components/premium/PremiumBenefits.tsx
@@ -29,14 +29,6 @@ export function PremiumBenefits() {
                 versions of Auxdibot features.
             </li>
             <li className={'flex items-center gap-1 font-open-sans'}>
-                <BsCheck className={'flex-shrink-0 text-2xl'} /> Special user
-                badge for your servers & profile on Auxdibot.
-            </li>
-            <li className={'flex items-center gap-1 font-open-sans'}>
-                <BsCheck className={'flex-shrink-0 text-2xl'} /> Higher priority
-                on Server Discovery (SOON)
-            </li>
-            <li className={'flex items-center gap-1 font-open-sans'}>
                 <BsCheck className={'flex-shrink-0 text-2xl'} /> Increased
                 limits for most features.
             </li>
@@ -47,14 +39,6 @@ export function PremiumBenefits() {
             <li className={'flex items-center gap-1 font-open-sans'}>
                 <BsCheck className={'flex-shrink-0 text-2xl'} /> Access to
                 premium-only utilities.
-            </li>
-            <li className={'flex items-center gap-1 font-open-sans'}>
-                <BsCheck className={'flex-shrink-0 text-2xl'} /> Access to
-                Server Groups (SOON).
-            </li>
-            <li className={'flex items-center gap-1 font-open-sans'}>
-                <BsCheck className={'flex-shrink-0 text-2xl'} /> Supporting the
-                future of Auxdibot.
             </li>
         </>
     );
